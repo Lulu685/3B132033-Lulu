4be96b9a218e189e57568f63060ecd715e26c7d6
Lulu685 <may1lulu2@gmail.com>
2025-12-19 10:08:58 +0800

    Delete addButtton.js

--- addButtton.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("addButton").onclick = function () {
-    const btn = document.createElement("button");
-    btn.textContent = "動態按鈕";
-    document.getElementById("container").appendChild(btn);
-}
