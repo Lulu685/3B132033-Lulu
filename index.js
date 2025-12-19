8c03a9869c4128f50c524d748bcb518ef8c21aeb
3b132033Lulu <may1lulu2@gmail.com>
2025-12-19 10:56:34 +0800

    Javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("text").textContent = "您好";
+const p = document.createElement("p");
+p.textContent = "動態文字段落";
+document.body.appendChild(p);
+document.getElementById("heading").innerHTML = "<h1>標題</h1>"
